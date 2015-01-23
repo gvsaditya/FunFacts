dd512c54ba231eae7a78bc40858e67e31737a018
Aditya Gollakota <gvsaditya@gmail.com>
2015-01-23 14:39:34 -0800

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by Aditya Gollakota on 1/23/15.
+//  Copyright (c) 2015 Aditya Gollakota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Aditya Gollakota on 1/23/15.
+//  Copyright (c) 2015 Aditya Gollakota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
